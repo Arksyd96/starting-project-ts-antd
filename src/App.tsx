0d278c02e616b08e6fe8d3bec286476de1a99af4
Arksyd96 <loupblanc1998@gmail.com>
2019-05-07 15:14:27 +0100

    Starting items

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component<any, any>{
+	public render() {
+		return (
+			<div>
+				là ou tout commence ...
+			</div>
+		);
+	}
+}
+
+export default App;
